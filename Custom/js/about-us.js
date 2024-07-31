f449867abd6e224e46ffb54f340e8ec129f5172d
HairyPotato69 <jimmyding9719@gmail.com>
2024-07-31 23:16:55 +0800

    Added filling animation

--- Custom/js/about-us.js
+++ Custom/js/about-us.js
@@ -1,5 +1,13 @@
+const about_us_section = document.getElementById('about-us');
+const style = window.getComputedStyle(about_us_section)
+
 const menuButton = document.querySelector('#menu-button')
 const rootElement = document.documentElement
+
+const primary =  '#151618';
+const accent_quarternary= '#2F452D';
+const accent_primary = '#C21807';
+const accent_secondary = '#FFD700';
 
 menuButton.addEventListener('click', () => {
   rootElement.toggleAttribute('menu-open')
@@ -47,4 +55,144 @@ jimmy_span.addEventListener('mouseover', () => {
 
 jimmy_span.addEventListener('mouseout', () => {
   jimmy_figure.classList.remove('hover-active');
+});
+
+let animation_playing = false;
+
+let timeline_m = anime.timeline({
+  easing: 'easeInOutQuad',
+  autoplay: false,
+  complete: function(){
+    animation_playing = false;
+  }
+})
+
+timeline_m.add({
+  targets: '.background-fill',
+  background: accent_quarternary,
+    width: [
+      function(el) {
+        return 0;
+      },
+      function (el){
+        return style.height;
+      }],
+
+    height: [
+      function(el){
+        return 0;
+      },
+      function(el){
+        return style.height;
+      }],
+
+    borderRadius:['50%', '50%'],
+    duration: 900
+
+})
+
+timeline_m.add({
+  targets: ['.about-malaysia p', '#malay', '#chinese', '#indian'],
+  color: accent_secondary,
+})
+
+timeline_m.add({
+  targets: '.background-fill',
+  background: accent_quarternary,
+    width: [        
+      function(el) {
+        return style.height;
+      },
+      function (el){
+        return style.width;
+      }],
+    height: [        
+      function(el) {
+      return style.height;
+    },
+    function (el){
+      return style.width;
+    }],
+    borderRadius:['50%', '0%'],
+  },850)
+timeline_m.add({
+    targets: '#about-us > .container',
+    borderColor: ['#000000','#FFD700']
+  })
+
+let timeline_c = anime.timeline({
+  easing: 'easeInOutQuad',
+  autoplay: false,
+  complete: function(){
+    animation_playing = false;
+  }
+})
+
+timeline_c.add({
+  targets: '.background-fill',
+  background: accent_primary,
+    width: [
+      function(el) {
+        return 0;
+      },
+      function (el){
+        return style.height;
+      }],
+
+    height: [
+      function(el){
+        return 0;
+      },
+      function(el){
+        return style.height;
+      }],
+
+    borderRadius:['50%', '50%'],
+    duration: 900
+
+})
+
+timeline_c.add({
+  targets: ['.about-malaysia p', '#malay', '#chinese', '#indian'],
+  color: primary,
+})
+
+timeline_c.add({
+  targets: '.background-fill',
+  background: accent_primary,
+    width: [        
+      function(el) {
+        return style.height;
+      },
+      function (el){
+        return style.width;
+      }],
+    height: [        
+      function(el) {
+      return style.height;
+    },
+    function (el){
+      return style.width;
+    }],
+    borderRadius:['50%', '0%'],
+  },850)
+timeline_c.add({
+    targets: '#about-us > .container',
+    borderColor: primary
+  })
+
+document.getElementById('malay').addEventListener('mouseover', function() {
+  if (!animation_playing){
+    animation_playing = true;
+    timeline_m
+  .play();
+  }
+});
+
+document.getElementById('chinese').addEventListener('mouseover', function() {
+  if (!animation_playing){
+    animation_playing = true;
+    timeline_c
+  .play();
+  }
 });
